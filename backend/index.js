2189d6fadf5985c5e113b89bf983fb1c46011119
Tino Schroter <tino.schroeter@gmail.com>
2022-03-12 20:13:39 +0100

    add post backend

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,18 @@ const morgan = require("morgan");
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('combined'))
 
 app.get("/", (req, res) => {
   res.send("Hallo test");
 });
+
+app.post("/boafrm/formNoticeRegister", (req, res) => {
+    console.log(req.body)
+    res.send("okay")
+})
 
 const port = process.env.PORT || 3000;
 
